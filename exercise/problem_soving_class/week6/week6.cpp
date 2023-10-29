199fe340ca21661a961320ab0ef3c9f60e63dd00
fanyang <2940276020@qq.com>
2023-10-29 18:59:04 +0800

    整理了一下week6

--- exercise/problem_soving_class/week6/week6.cpp
+++ exercise/problem_soving_class/week6/week6.cpp
@@ -1,13 +1,28 @@
+// 相对上周而言，此次的类写得更合理，结构以及各函数更清晰
+
 #include "solute.h"
 #include "format_poly.h"
+#include <fstream>
+#include <iomanip>
+
+double get_four_pre(double x)
+{
+    int m = 100000 * x;
+    if (m % 100 == 99)
+        return x;
+    int y = 10000 * x;
+    double z = y;
+    return z / 10000;
+}
 
 int main()
 {
     string line;
     vector<string> illegel;
     solute solution;
+    fstream file("test.txt");
     cout << "Legal Equations:" << endl;
-    while (getline(cin, line))
+    while (getline(file, line))
     {
         tranform tran(line);
         vector<string> vari = tran.get_vari();
@@ -26,13 +41,14 @@ int main()
         table = solution.get_solution();
         for (map<string, double>::iterator it = table.begin(); it != table.end(); it++)
         {
-            cout << it->first << ':' << it->second << endl;
+            cout << it->first << ':' << fixed << setprecision(4) << get_four_pre(it->second) << endl;
         }
     }
     catch (const std::exception &e)
     {
         cout << e.what();
     }
+    cout << "Illegal Equations:" << endl;
     for (auto p : illegel)
     {
         cout << p << endl;
